43a3b70b231aca1b1b2a3482c6c033c56d915633
RoraZhu <rz119@duke.edu>
2022-02-05 21:57:27 -0500

    new

--- testapp/github_info_header.py
+++ testapp/github_info_header.py
@@ -9,8 +9,8 @@ def github_info_header():
     return html.Div([
         html.P("Current working directory: {0}".format(getcwd())),
         html.P(repo.head.ref.path),
-        # html.P(repo.head.object.hexsha),
-        # html.P(repo.head.object.hexsha),
+        html.P(repo.head.object.hexsha),
+        html.P(repo.head.object.hexsha),
         html.P(repo.head.commit.message),
         html.P(repo.head.commit.author.name),
         html.P(
